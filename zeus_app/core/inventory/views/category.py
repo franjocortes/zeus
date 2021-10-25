845f12a1d82a54f132ef15d0d3b3bd105ba2b4de
franjo <franjo.cortes@gmail.com>
2021-10-24 22:12:47 -0300

    last changes...

--- zeus_app/core/inventory/views/category.py
+++ zeus_app/core/inventory/views/category.py
@@ -4,7 +4,6 @@ from django.views.generic import ListView, CreateView
 from core.inventory.forms import CategoryForm
 from core.inventory.models import Categories
 
-
 class CategoryListView(ListView):
     template_name = 'category/list.html'
     model = Categories
@@ -13,13 +12,9 @@ class CategoryListView(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Categorías'
         context['title2'] = 'Listado'
+        context['title_form'] = 'Categoria'
+        context['form'] = CategoryForm()
         # context['company'] = CompanyData.objects.get(pk=1).name
         # context['formpage'] = reverse_lazy('inventory:category_new')
         return context
 
-
-class CategoryCreateView(CreateView):
-    model = Categories
-    form_class = CategoryForm
-    template_name = 'common/create.html'
-    success_url = reverse_lazy('inventory:category_list')
---
+++ zeus_app/static/js/my_msg.js
@@ -0,0 +1,71 @@
+const message_error = (data) => {
+    let html = '';
+    if (typeof (data) === 'object') {
+        $.each(data, function (key, value) {
+            html += `${value}<br>`;
+        });
+    } else {
+        html = `<p>${data}</p>`;
+    }
+    Swal.fire({
+        title: 'Se han encontrado errores',
+        html: html,
+        icon: 'error'
+    });
+};
+
+
+// msj: El mensaje que quiero mostrar
+// url: si quiero que me lleve a otro lado cuando termine de mostrar el mensaje
+// function_ok: Funcion que quiero que se ejecute cuando termine de mostrar el mensaje
+const message_info = (msg = 'Mensaje', url = '', function_ok = null) => {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: msg,
+        showConfirmButton: false,
+        timer: 1500
+    }).then((r) => {
+        if (url !== '') {
+            location.href = url;
+        }
+        if (function_ok !== null) {
+            function_ok();
+        }
+    });
+};
+
+
+const message_question = (msg = 'Mensaje', function_ok) => {
+    Swal.fire({
+        title: '¿Está seguro?',
+        text: msg,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            function_ok();
+        }
+    })
+};
+
+const submitWithAjax = (title, message, url, parameters, callback) => {
+    Swal.fire({
+        title: title,
+        text: message,
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: "Si",
+        cancelButtonText: "No",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            callback();
+        }
+    })
+}
